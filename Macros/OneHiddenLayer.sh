ebfb19e5b4ba18847c3af3664ab69b9f7c3f2edc
ryanwongsa <ryanwong1993@gmail.com>
2015-09-24 19:02:43 +0200

    Refactored code

---
+++ Macros/OneHiddenLayer.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+./pid_gen ToUseData.txt 1 4 1 100000 > OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 4 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 4 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 6 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 6 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 6 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 8 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 8 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 8 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 10 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 10 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 10 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 12 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 12 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 12 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 14 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 14 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 14 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 16 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 16 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 16 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 18 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 18 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 18 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 20 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 20 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 20 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 22 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 22 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 22 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 24 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 24 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 24 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 26 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 26 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 26 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 28 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 28 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 28 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 30 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 30 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 30 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 32 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 32 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 32 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 34 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 34 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 34 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 36 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 36 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 36 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 38 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 38 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 38 1 100000 >> OneHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 1 40 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 40 1 100000 >> OneHiddenLayerData.txt
+./pid_gen ToUseData.txt 1 40 1 100000 >> OneHiddenLayerData.txt
---
+++ Macros/TwoHiddenLayer.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+./pid_gen ToUseData.txt 2 4 1 100000 > TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 4 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 4 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 6 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 6 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 6 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 8 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 8 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 8 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 10 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 10 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 10 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 12 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 12 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 12 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 14 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 14 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 14 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 16 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 16 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 16 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 18 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 18 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 18 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 20 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 20 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 20 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 22 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 22 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 22 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 24 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 24 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 24 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 26 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 26 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 26 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 28 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 28 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 28 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 30 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 30 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 30 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 32 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 32 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 32 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 34 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 34 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 34 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 36 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 36 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 36 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 38 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 38 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 38 1 100000 >> TwoHiddenLayerData.txt
+
+./pid_gen ToUseData.txt 2 40 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 40 1 100000 >> TwoHiddenLayerData.txt
+./pid_gen ToUseData.txt 2 40 1 100000 >> TwoHiddenLayerData.txt
